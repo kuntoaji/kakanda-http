0775b9734b760af03dd7b81c4a4a7ce49424fff1
Kunto Aji. K <kuntoaji@kaklabs.com>
2017-01-08 08:35:30 +0000

    configure react, babel, and webpack with yarn

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
+}
